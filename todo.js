94d4f15d9ce98111acc77fd31f8023dc96a90e38
TOMAS URIEL BLASER <46024452@ort>
2022-05-30 10:34:49 -0300

    ..

--- todo.js
+++ todo.js
@@ -1,16 +1,23 @@
 let listatarea=[]
 let ultimo = 0
+let titulo = '--'
+let clase = ''
+
+
 
 let ObtenerElemento=(titulo) => {
+    
     let tarea={
         id: ultimo,
         titulo:titulo,
         status:false
     }
+
     if(tarea.titulo!=''){
         listatarea.push(tarea);
         let ul=document.getElementById("lista");
-        ul.innerHTML+=`<li><input type="checkbox" id="${ultimo}" onclick=marcar(id)><h10> ${titulo} </h10></input></li>`
+        ul.innerHTML+=`<li><input type="checkbox" id="${ultimo}" onclick=marcar(id)><h10 class=${clase}> ${titulo} </h10></input></li>`
+        console.log(tarea)
         ultimo++;
     }else{
         alert('Por favor, ingrese algo');
@@ -19,14 +26,18 @@ let ObtenerElemento=(titulo) => {
 }
 
 let marcar = (id) => {
-    listatarea[id].estado = true;
-    refrescar();
-}
+    listatarea[id].status = true;
+    
+    if(listatarea[id].status === true)
+    {
+        clase='tachado'
+        for(i=0; i<=ultimo; i++)
+        {
+            if(listatarea.[id].status===true){
 
-refrescar = () =>{
-    let lista=document.getElementById('lista')
-    lista.innerHTML+=``
-    for(i=0; i<idlista+1; i++){
-        lista.innerHTML+=`<li><input type="checkbox" id=${listatarea[idlista].id} style="text-decoration: line-through">${listatarea[idlista].titulo}</input></li>`
+                ul.innerHTML=`<input type="checkbox" id="${id}"><h10 class=${clase}> ${listatarea[id].titulo} </h10></input>`
+                console.log(tarea)
+            }
+        }
     }
 }
